c3d09d31497368cd3cb5062e1698423c623a044e
qinvz <sun.houtao@outlook.com>
2019-05-01 17:18:20 +0800

    拼写错误

--- rest/models/AnimateModel.js
+++ rest/models/AnimateModel.js
@@ -1,70 +1,80 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
-const AnimateSchema = new Schema({
-    title: {                                          // 标题
-      type: String, 
-      required: true 
-    },                 
-    slug: {                                           // 别名, 唯一标识符
-      type: String, 
-      required: true, 
+const AnimateSchema = new Schema(
+  {
+    title: {
+      // 标题
+      type: String,
+      required: true
+    },
+    slug: {
+      // 别名, 唯一标识符
+      type: String,
+      required: true,
       index: true,
       unique: true,
-      trim:true
+      trim: true
     },
-    author:Schema.Types.ObjectId,
+    author: Schema.Types.ObjectId,
     status: {
-      type:String,
-      enum:['draft','publish','reject'],
-      default:'draft'
+      type: String,
+      enum: ["draft", "publish", "reject"],
+      default: "draft"
     },
-    information:{                                     // 动漫详情
-      introduce:{type:String, default:''},            // 简介
-      staff:{type:String, default:''},                // 工作人员
-      actor:{type:String, default:''},                // 声优
-      firstPlay:{type:String, default:'20160606'},    // 首播
-      isUpdate:{ type:Boolean,default:false},         // 是否连载
-      updateDay:{ type: Number,default:0},            // 周几播放
-      rateStar:{ type: Number,default:4},             // 评分星级
-      rateCount: { type: Number,default:1000},        // 评分人数
-      impression:{ type: String,default:''},          // 印象
-      episodeCount:{type:Number,default:0},           // 总集数
+    information: {
+      // 动漫详情
+      introduce: { type: String, default: "" }, // 简介
+      staff: { type: String, default: "" }, // 工作人员
+      actor: { type: String, default: "" }, // 声优
+      firstPlay: { type: String, default: "20160606" }, // 首播
+      isUpdate: { type: Boolean, default: false }, // 是否连载
+      updateDay: { type: Number, default: 0 }, // 周几播放
+      rateStar: { type: Number, default: 4 }, // 评分星级
+      rateCount: { type: Number, default: 1000 }, // 评分人数
+      impression: { type: String, default: "" }, // 印象
+      eposideCount: { type: Number, default: 0 } // 总集数
     },
-    play:{
+    play: {
       kind: {
-        type:String,
-        enum:['mp4','m3u8','php'],
-        default:'mp4'
-      },                                              // 播放类型
-      level: { type: Number,default:0},               // 等级限定
-      linkPrefix:{ type: String,default:''},          // 链接前缀
-      downTitle:{ type: String,default:''},           // 下载标题
-      downLink:{ type: String,default:''}             // 下载链接
+        type: String,
+        enum: ["mp4", "m3u8", "php"],
+        default: "mp4"
+      }, // 播放类型
+      level: { type: Number, default: 0 }, // 等级限定
+      linkPrefix: { type: String, default: "" }, // 链接前缀
+      downTitle: { type: String, default: "" }, // 下载标题
+      downLink: { type: String, default: "" } // 下载链接
     },
-    eposide:[{                                        // 剧集信息
-      season:String,                                  // 季数
-      list:[{
-        title:String,                                 // 标题
-        link:String,                                  // 链接
-        danmu:String                                  // 弹幕ID
-      }]
-    }],
-    cover:{
-      vertical:{ type: String,default:''},            // 竖向大图
-      horizontal:{ type: String,default:''},          // 横向大图
+    eposide: [
+      {
+        // 剧集信息
+        season: String, // 季数
+        list: [
+          {
+            title: String, // 标题
+            link: String, // 链接
+            danmu: String // 弹幕ID
+          }
+        ]
+      }
+    ],
+    cover: {
+      vertical: { type: String, default: "" }, // 竖向大图
+      horizontal: { type: String, default: "" } // 横向大图
     },
-    category:{
-      area: Schema.Types.ObjectId,                    // 地区
-      kind: Schema.Types.ObjectId,                    // 类型
-      year: Schema.Types.ObjectId,                    // 年份
-      tag: [{ type: String }],                        // 标签
+    category: {
+      area: Schema.Types.ObjectId, // 地区
+      kind: Schema.Types.ObjectId, // 类型
+      year: Schema.Types.ObjectId, // 年份
+      tag: [{ type: String }] // 标签
     },
-    addons:Schema.Types.Mixed
-  },{
+    addons: Schema.Types.Mixed
+  },
+  {
     timestamps: true
   }
 );
 
-module.exports=mongoose.model('Animate', AnimateSchema);
+module.exports = mongoose.model("Animate", AnimateSchema);
