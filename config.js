c2aaeb96ab9e2159ccf9ed75b865713916b982c7
qinvz <sun.houtao@outlook.com>
2019-06-15 14:30:54 +0800

    mongodb

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   author: "qinvz",
   version: "v1",
   mongodb: {
-    //uri:
-    //"mongodb://qinvideo:Op6bg0PzQnVuTSjGuK0TkHJyUtGkTtQK@localhost:27017/qinvideo"
-    uri: "mongodb://localhost:27017/qinvideo"
+    uri:
+      "mongodb://qinvideo:Op6bg0PzQnVuTSjGuK0TkHJyUtGkTtQK@localhost:27017/qinvideo"
+    // uri: "mongodb://localhost:27017/qinvideo"
   },
   salt: "qinmei",
   tokenSecret: "qinmei"
