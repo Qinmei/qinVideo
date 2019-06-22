cb2d686338d6649e7132ac608de1c62052d079e8
qinvz <sun.houtao@outlook.com>
2019-06-23 00:36:03 +0800

    bug fix

--- rest/controllers/commentController.js
+++ rest/controllers/commentController.js
@@ -229,8 +229,7 @@ class commentController {
     if (user.level < 100) {
       // commentQuery.status = "publish";
       arrLookup = [];
-      belong &&
-        (commentQuery.belong = { $regex: `/${belong}$/`, $options: "$i" });
+      belong && (commentQuery.belong = { $regex: `/${belong}$/` });
     }
 
     const data = await CommentModel.aggregate([
