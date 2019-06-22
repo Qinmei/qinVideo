fbc69347cef7fc022eb28f8372c9dadbaf4b0c45
qinvz <sun.houtao@outlook.com>
2019-06-22 12:07:30 +0800

    bug fix

--- rest/utils/init.js
+++ rest/utils/init.js
@@ -1,13 +1,15 @@
 const { UserModel, ConfigModel } = require("../models/index");
 const mongoose = require("mongoose");
 const stringRandom = require("string-random");
+const { MD5 } = require("./common");
+const configInit = require("../../config");
 
 module.exports = async () => {
   const total = await UserModel.countDocuments();
   if (total === 0) {
     await UserModel.create({
       name: "qinvideo",
-      password: "1f4981475a583e8f323dcb429b38264a",
+      password: MD5(configInit.salt + "e10adc3949ba59abbe56e057f20f883e"),
       level: 100,
       email: "test@qinvideo.org",
       refreshToken: new mongoose.Types.ObjectId().toString() + stringRandom(16)
