4dcd0f28324c90aa34caa8cdd492381f1511d5e6
qinvz <sun.houtao@outlook.com>
2019-09-28 00:07:23 +0800

    后台流程优化

--- public/backend/service-worker.js
+++ public/backend/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/backend/precache-manifest.cc08d4fcb05e367336a38f3b715ad607.js", "/backend/workbox-v3.6.3/workbox-sw.js");
+importScripts("/backend/precache-manifest.064f87d44b1362f34316cdf57fa5110f.js", "/backend/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/backend/workbox-v3.6.3"});
 /* globals workbox */
 /* eslint-disable no-restricted-globals */
