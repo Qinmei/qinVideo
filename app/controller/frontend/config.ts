05ff33ad8d05066f7e235645e6caa991e786d40e
qinvz <sun.houtao@outlook.com>
2019-12-09 22:24:34 +0800

    config修改

--- app/controller/frontend/config.ts
+++ app/controller/frontend/config.ts
@@ -1,28 +1,24 @@
 import { Controller } from 'egg';
 
 interface ConfigInfo {
-    pcIndex: [string];
-    animeIndex: [string];
-    comicIndex: [string];
-    h5Index: [string];
+    pcIndex: string[];
+    animeIndex: string[];
+    comicIndex: string[];
+    h5Index: string[];
 }
 
 class ConfigController extends Controller {
     async info() {
         const { ctx, service } = this;
         const result: ConfigInfo | number = await service.config.simpleInfo().catch(() => 15000);
-        if (result !== 15000) {
+        if (typeof result !== 'number') {
             const arr = ['pcIndex', 'animeIndex', 'comicIndex', 'h5Index'];
-            const list: [string] = [''];
-            arr.map((item: string) => list.push(...result[item]));
-
-            console.log(arr);
+            const list = [...result.pcIndex].filter((item: string) => !/new/.test(item));
 
             const cates = await service.category.list(list);
 
-            console.log(cates);
-
             arr.map((item: any) => {
+                if (!result[item]) result[item] = [];
                 result[item] = result[item].map((ele: string) => {
                     const filters = cates.filter((single: any) => single.id === ele);
                     return filters.length > 0 ? filters[0] : ele;
--- app/service/category.ts
+++ app/service/category.ts
@@ -1,52 +1,52 @@
 import { Service } from 'egg';
 
 class CategoryService extends Service {
-	async query({ page, size, sortBy, sortOrder, type, title }) {
-		const skip: number = (page - 1) * size;
-		const limit: number = size;
-		const query: any = {};
-		type && (query.type = type);
-		title && (query.name = { $regex: title, $options: '$i' });
+    async query({ page, size, sortBy, sortOrder, type, title }) {
+        const skip: number = (page - 1) * size;
+        const limit: number = size;
+        const query: any = {};
+        type && (query.type = type);
+        title && (query.name = { $regex: title, $options: '$i' });
 
-		const result = await this.ctx.model.Category.find(query)
-			.sort({ [sortBy]: sortOrder, _id: -1 })
-			.skip(skip)
-			.limit(limit);
+        const result = await this.ctx.model.Category.find(query)
+            .sort({ [sortBy]: sortOrder, _id: -1 })
+            .skip(skip)
+            .limit(limit);
 
-		const total = await this.ctx.model.Category.find(query).countDocuments();
+        const total = await this.ctx.model.Category.find(query).countDocuments();
 
-		return {
-			list: result,
-			total,
-		};
-	}
+        return {
+            list: result,
+            total,
+        };
+    }
 
-	async info(id: string) {
-		const data = this.ctx.model.Category.findById(id);
-		return data;
-	}
+    async info(id: string) {
+        const data = this.ctx.model.Category.findById(id);
+        return data;
+    }
 
-	async create(data: any) {
-		const result = await this.ctx.model.Category.create(data);
-		return result;
-	}
+    async create(data: any) {
+        const result = await this.ctx.model.Category.create(data);
+        return result;
+    }
 
-	async update(ids: string[], data: any) {
-		const query = ids.length > 0 ? { _id: { $in: ids } } : {};
-		const result = await this.ctx.model.Category.updateMany(query, { $set: data });
-		return result;
-	}
+    async update(ids: string[], data: any) {
+        const query = ids.length > 0 ? { _id: { $in: ids } } : {};
+        const result = await this.ctx.model.Category.updateMany(query, { $set: data });
+        return result;
+    }
 
-	async destroy(ids: string[], type?: string) {
-		const query = ids.length > 0 ? { _id: { $in: ids } } : { type };
-		const result = await this.ctx.model.Category.deleteMany(query);
-		return result;
-	}
+    async destroy(ids: string[], type?: string) {
+        const query = ids.length > 0 ? { _id: { $in: ids } } : { type };
+        const result = await this.ctx.model.Category.deleteMany(query);
+        return result;
+    }
 
-	async list(list:[string]) {
-		const data = this.ctx.model.Category.find({ _id: { $in: list } });
-		return data;
-	}
+    async list(list: string[]) {
+        const data = this.ctx.model.Category.find({ _id: { $in: list } });
+        return data;
+    }
 }
 
 export default CategoryService;
