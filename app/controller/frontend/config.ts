cbc2e9d982891b4574fe6d6539e3e4587b02ed37
sunhoutao <sun.houtao@intellig.com>
2019-12-11 15:04:06 +0800

    一些调整

--- app/controller/frontend/config.ts
+++ app/controller/frontend/config.ts
@@ -3,7 +3,7 @@ import { Controller } from 'egg';
 class ConfigController extends Controller {
     async info() {
         const { ctx, service } = this;
-        const result = await service.config.simpleInfo().catch(() => 15000);
+        const result = await service.config.simpleInfo().catch((err) => console.log(err));
         ctx.helper.send(result);
     }
 }
