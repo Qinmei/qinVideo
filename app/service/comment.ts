86b18d11092490dd4ee6ad6c85a8dc5efa6e6388
sunhoutao <sun.houtao@intellig.com>
2019-12-13 19:16:30 +0800

    一些调整

--- app/service/comment.ts
+++ app/service/comment.ts
@@ -1,93 +1,123 @@
 import { Service } from 'egg';
 
 class CommentService extends Service {
-	async query({ page, size, sortBy, sortOrder, title, target, status }) {
-		const skip: number = (page - 1) * size;
-		const limit: number = size;
+    async query({ page, size, sortBy, sortOrder, title, target, status }) {
+        const skip: number = (page - 1) * size;
+        const limit: number = size;
 
-		const query: any = {};
-		title && (query.content = { $regex: title, $options: '$i' });
-		status && (query.status = status);
-		target && (query.target = target);
+        const query: any = {};
+        title && (query.content = { $regex: title, $options: '$i' });
+        status && (query.status = status);
+        target && (query.target = target);
 
-		const result = await this.ctx.model.Comment.find(query)
-			.populate('countLike')
-			.sort({ [sortBy]: sortOrder, _id: -1 })
-			.skip(skip)
-			.limit(limit)
-			.populate({ path: 'target', select: '_id title' })
-			.populate({ path: 'author', select: 'name avatar level introduce background' })
-			.populate({
-				path: 'replyTo',
-				select: 'name avatar level introduce background',
-			});
+        const result = await this.ctx.model.Comment.find(query)
+            .populate('countLike')
+            .sort({ [sortBy]: sortOrder, _id: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate({ path: 'target', select: '_id title' })
+            .populate({ path: 'author', select: 'name avatar level introduce background' })
+            .populate({
+                path: 'replyTo',
+                select: 'name avatar level introduce background',
+            });
 
-		const total = await this.ctx.model.Comment.find(query).countDocuments();
+        const total = await this.ctx.model.Comment.find(query).countDocuments();
 
-		return {
-			list: result,
-			total,
-		};
-	}
+        return {
+            list: result,
+            total,
+        };
+    }
 
-	async info({ page, size, sortBy, sortOrder, title, target, status }) {
-		const skip: number = (page - 1) * size;
-		const limit: number = size;
+    async info({ page, size, sortBy, sortOrder, title, target, status }) {
+        const skip: number = (page - 1) * size;
+        const limit: number = size;
 
-		const query: any = {
-			parent: null,
-		};
-		title && (query.content = { $regex: title, $options: '$i' });
-		status && (query.status = status);
-		target && (query.target = target);
+        const query: any = {
+            parent: null,
+        };
+        title && (query.content = { $regex: title, $options: '$i' });
+        status && (query.status = status);
+        target && (query.target = target);
 
-		const result = await this.ctx.model.Comment.find(query)
-			.populate('countLike')
-			.sort({ [sortBy]: sortOrder, _id: -1 })
-			.skip(skip)
-			.limit(limit)
-			.populate({
-				path: 'children',
-				populate: [
-					{
-						path: 'author',
-						select: 'name avatar level introduce background',
-					},
-					{
-						path: 'countLike',
-					},
-					{
-						path: 'replyTo',
-						select: 'name avatar level introduce background',
-					},
-				],
-			})
-			.populate({ path: 'author', select: 'name avatar level introduce background' });
+        const result = await this.ctx.model.Comment.find(query)
+            .populate('countLike')
+            .sort({ [sortBy]: sortOrder, _id: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate({
+                path: 'children',
+                populate: [
+                    {
+                        path: 'author',
+                        select: 'name avatar level introduce background',
+                    },
+                    {
+                        path: 'countLike',
+                    },
+                    {
+                        path: 'replyTo',
+                        select: 'name avatar level introduce background',
+                    },
+                ],
+            })
+            .populate({ path: 'author', select: 'name avatar level introduce background' });
 
-		const total = await this.ctx.model.Comment.find(query).countDocuments();
+        const total = await this.ctx.model.Comment.find(query).countDocuments();
 
-		return {
-			list: result,
-			total,
-		};
-	}
+        return {
+            list: result,
+            total,
+        };
+    }
 
-	async create(data: any) {
-		const result = await this.ctx.model.Comment.create(data);
-		return result;
-	}
+    async create(data: any) {
+        const result = await this.ctx.model.Comment.create(data);
+        return result;
+    }
 
-	async update(ids: string[], data: any) {
-		const query = ids.length > 0 ? { _id: { $in: ids } } : {};
-		const result = await this.ctx.model.Comment.updateMany(query, { $set: data });
-		return result;
-	}
+    async update(ids: string[], data: any) {
+        const query = ids.length > 0 ? { _id: { $in: ids } } : {};
+        const result = await this.ctx.model.Comment.updateMany(query, { $set: data });
+        return result;
+    }
 
-	async destroy(ids: string[]) {
-		const query = ids.length > 0 ? { _id: { $in: ids } } : {};
-		const result = await this.ctx.model.Comment.deleteMany(query);
-		return result;
-	}
+    async destroy(ids: string[]) {
+        const query = ids.length > 0 ? { _id: { $in: ids } } : {};
+        const result = await this.ctx.model.Comment.deleteMany(query);
+        return result;
+    }
+
+    // frontend
+    async list({ page, size, sortBy, sortOrder, target, status }) {
+        const skip: number = (page - 1) * size;
+        const limit: number = size;
+
+        const query: any = {
+            target,
+        };
+        status && (query.status = status);
+
+        const result = await this.ctx.model.Comment.find(query)
+            .populate('countLike')
+            .sort({ [sortBy]: sortOrder, _id: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate({ path: 'target', select: '_id title' })
+            .populate({ path: 'author', select: 'name avatar level introduce background' })
+            .populate({
+                path: 'replyTo',
+                select: 'name avatar level introduce background',
+            });
+
+        const total = await this.ctx.model.Comment.find(query).countDocuments();
+
+        return {
+            list: result,
+            total,
+        };
+    }
 }
 
 export default CommentService;
