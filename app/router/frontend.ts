a5851e485ac8bd73994391720af8ae59d5ff1afe
qinvz <sun.houtao@outlook.com>
2020-01-19 00:05:30 +0800

    调整请求方式

--- app/router/frontend.ts
+++ app/router/frontend.ts
@@ -28,12 +28,12 @@ export default (app: Application) => {
     router.get(`${api}/user/:id/base`, auth(0), controller.frontend.user.baseInfo);
     router.get(`${api}/user/:id/own/:type`, auth(0), controller.frontend.user.own);
     router.get(`${api}/user/like/:type`, auth(1), controller.frontend.user.like);
+    router.get(`${api}/user/history/:type`, auth(1), controller.frontend.user.history);
     router.get(`${api}/user/order/list`, auth(1), controller.frontend.user.orderList);
     router.post(`${api}/user/action`, auth(1), controller.frontend.user.relation);
     router.post(`${api}/user/edit`, auth(1), controller.frontend.user.edit);
     router.post(`${api}/user/pay`, auth(1), controller.frontend.user.pay);
     router.post(`${api}/user/order`, auth(1), controller.frontend.user.order);
-    router.post(`${api}/user/history/:type`, auth(1), controller.frontend.user.history);
 
     // animate
     router.get(`${api}/animates`, auth(0), controller.frontend.animate.query);
