bea2f4f3cee0f436e4297ea34790c8fe0cf59556
qinvz <sun.houtao@outlook.com>
2020-07-11 23:00:12 +0800

    errorCode

--- app/controller/frontend/auth.ts
+++ app/controller/frontend/auth.ts
@@ -1,218 +1,224 @@
-import { Controller } from 'egg';
-import * as jwt from 'jsonwebtoken';
-import * as uuidv4 from 'uuid/v4';
+import { Controller } from "egg";
+import * as jwt from "jsonwebtoken";
+import * as uuidv4 from "uuid/v4";
 
 class AuthController extends Controller {
-    async info() {
-        const { ctx, service } = this;
-        const userId = ctx.state.user.id;
+  async info() {
+    const { ctx, service } = this;
+    const userId = ctx.state.user.id;
 
-        if (!userId) return ctx.helper.status(403);
+    if (!userId) return ctx.helper.status(403);
 
-        const result = await service.user.info(userId).catch(() => 11000);
-        ctx.helper.send(result);
+    const result = await service.user.info(userId).catch(() => 11000);
+    ctx.helper.send(result);
+  }
+
+  async existByName(name: string) {
+    const { ctx, service } = this;
+    const nameResult = await service.user.exist({ name });
+
+    if (!nameResult) {
+      ctx.helper.error(10006);
+    }
+    return nameResult;
+  }
+
+  async existByEmail(email: string) {
+    const { ctx, service } = this;
+    const emailResult = await service.user.exist({ email });
+
+    if (!emailResult) {
+      ctx.helper.error(10012);
+    }
+    return emailResult;
+  }
+
+  async noExistByName(oldName: string) {
+    const { ctx, service } = this;
+    const name = oldName.replace(/\s/g, "");
+
+    if (name.replace(/[\u4e00-\u9fa5]/g, "aa").length < 6) {
+      ctx.helper.error(10020);
     }
 
-    async existByName(name: string) {
-        const { ctx, service } = this;
-        const nameResult = await service.user.exist({ name });
-
-        if (!nameResult) {
-            ctx.helper.error(10006);
-        }
-        return nameResult;
+    if (name.replace(/[\u4e00-\u9fa5]/g, "aa").length > 30) {
+      ctx.helper.error(10021);
     }
 
-    async existByEmail(email: string) {
-        const { ctx, service } = this;
-        const emailResult = await service.user.exist({ email });
+    const nameResult = await service.user.exist({ name });
 
-        if (!emailResult) {
-            ctx.helper.error(10012);
-        }
-        return emailResult;
+    if (nameResult) {
+      ctx.helper.error(10005);
     }
 
-    async noExistByName(oldName: string) {
-        const { ctx, service } = this;
-        const name = oldName.replace(/\s/g, '');
+    const sensitive = await service.utils.isSensitiveWord(name);
 
-        if (name.replace(/[\u4e00-\u9fa5]/g, 'aa').length < 6) {
-            ctx.helper.error(11020);
-        }
+    if (sensitive) {
+      ctx.helper.error(10019);
+    }
+    return true;
+  }
 
-        if (name.replace(/[\u4e00-\u9fa5]/g, 'aa').length > 30) {
-            ctx.helper.error(11021);
-        }
+  async noExistByEmail(email: string) {
+    const { ctx, service } = this;
 
-        const nameResult = await service.user.exist({ name });
-
-        if (nameResult) {
-            ctx.helper.error(10005);
-        }
-
-        const sensitive = await service.utils.isSensitiveWord(name);
-
-        if (sensitive) {
-            ctx.helper.error(10019);
-        }
-        return true;
+    if (email.length > 30) {
+      ctx.helper.error(10022);
     }
 
-    async noExistByEmail(email: string) {
-        const { ctx, service } = this;
-
-        if (email.length > 30) {
-            ctx.helper.error(10022);
-        }
-
-        if (!ctx.helper.validateEmail(email)) {
-            ctx.helper.error(10023);
-        }
-
-        const emailResult = await service.user.exist({ email });
-
-        if (emailResult) {
-            ctx.helper.error(10007);
-        }
-        return emailResult;
+    if (!ctx.helper.validateEmail(email)) {
+      ctx.helper.error(10023);
     }
 
-    async generateToken(userInfo: any) {
-        const { ctx } = this;
-        const { _id, email, name, level, score, avatar, background, introduce } = userInfo;
-        const token = jwt.sign(
-            { id: _id, name, email, level, score, avatar, background, introduce },
-            ctx.app.config.tokenSecret,
-            { expiresIn: '1d' }
-        );
-        return token;
+    const emailResult = await service.user.exist({ email });
+
+    if (emailResult) {
+      ctx.helper.error(10007);
     }
+    return emailResult;
+  }
 
-    async login() {
-        const { ctx, service } = this;
-        const data = ctx.request.body;
+  async generateToken(userInfo: any) {
+    const { ctx } = this;
+    const { _id, email, name, level, score, avatar, background, introduce } =
+      userInfo;
+    const token = jwt.sign(
+      { id: _id, name, email, level, score, avatar, background, introduce },
+      ctx.app.config.tokenSecret,
+      { expiresIn: "1d" },
+    );
+    return token;
+  }
 
-        ctx.helper.validate('login', data);
+  async login() {
+    const { ctx, service } = this;
+    const data = ctx.request.body;
 
-        const { name, password } = data;
-        await this.existByName(name);
-        data.password = ctx.helper.MD5(ctx.app.config.salt + password);
+    ctx.helper.validate("login", data);
 
-        const result = await service.user.exist(data).catch(() => 10008);
+    const { name, password } = data;
+    await this.existByName(name);
+    data.password = ctx.helper.MD5(ctx.app.config.salt + password);
 
-        if (!result) ctx.helper.error(10008);
+    const result = await service.user.exist(data).catch(() => 10008);
 
-        const { refreshToken } = result;
+    if (!result) ctx.helper.error(10008);
 
-        const token = await this.generateToken(result);
+    const { refreshToken } = result;
 
-        ctx.helper.success({ token, refreshToken });
+    const token = await this.generateToken(result);
+
+    ctx.helper.success({ token, refreshToken });
+  }
+
+  async register() {
+    const { ctx, service } = this;
+    const data = ctx.request.body;
+
+    ctx.helper.validate("register", data);
+
+    const { name, email, password } = data;
+    await this.noExistByName(name);
+    await this.noExistByEmail(email);
+    data.password = ctx.helper.MD5(ctx.app.config.salt + password);
+
+    const config = await service.config.cacheInfo();
+    data.avatar = config.avatar;
+    data.background = config.background;
+
+    const result = await service.user.create(data).catch(() => 10009);
+
+    ctx.helper.send(result);
+  }
+
+  async refreshtoken() {
+    const { ctx, service } = this;
+    const { refreshToken } = ctx.request.body;
+    const userInfo = await service.user.exist({ refreshToken });
+
+    if (userInfo) {
+      const token = await this.generateToken(userInfo);
+
+      ctx.helper.success({ token });
+    } else {
+      ctx.helper.status(403);
     }
+  }
 
-    async register() {
-        const { ctx, service } = this;
-        const data = ctx.request.body;
+  async resetPasswordMail() {
+    const { ctx, service } = this;
+    const { email } = ctx.request.body;
 
-        ctx.helper.validate('register', data);
+    await this.existByEmail(email);
 
-        const { name, email, password } = data;
-        await this.noExistByName(name);
-        await this.noExistByEmail(email);
-        data.password = ctx.helper.MD5(ctx.app.config.salt + password);
+    const userInfo = await service.user.exist({ email });
 
-        const config = await service.config.cacheInfo();
-        data.avatar = config.avatar;
-        data.background = config.background;
+    const { _id, name } = userInfo;
 
-        const result = await service.user.create(data).catch(() => 10009);
+    const token = await service.utils.generateCode(_id, 7200);
 
-        ctx.helper.send(result);
-    }
+    service.utils.sendMail({
+      to: email,
+      subject: "重置密码",
+      text: "正文如下:",
+      html:
+        `<h3>亲爱的${name}:<h3><p>您正在进行重置密码的操作,如果不是您本人所为请忽略此邮件,确认重置密码请复制点击下方验证码, 有效期两小时:</p><p style='margin-left:30px;font-size:20px'>${token}</p>`,
+    });
 
-    async refreshtoken() {
-        const { ctx, service } = this;
-        const { refreshToken } = ctx.request.body;
-        const userInfo = await service.user.exist({ refreshToken });
+    ctx.helper.success("send success");
+  }
 
-        if (userInfo) {
-            const token = await this.generateToken(userInfo);
+  async resetPasswordAuth() {
+    const { ctx, service } = this;
+    const { password, token } = ctx.request.body;
 
-            ctx.helper.success({ token });
-        } else {
-            ctx.helper.status(403);
-        }
-    }
+    let result = 10011;
+    try {
+      const id = await service.utils.authCode(token);
+      if (!id) return ctx.helper.error(10016);
 
-    async resetPasswordMail() {
-        const { ctx, service } = this;
-        const { email } = ctx.request.body;
+      const newPass = ctx.helper.MD5(ctx.app.config.salt + password);
+      const refreshToken = uuidv4();
+      result = await service.user.update(
+        [id],
+        { password: newPass, refreshToken },
+      ).catch(() => 10011);
+    } catch (err) {}
 
-        await this.existByEmail(email);
+    ctx.helper.send(result);
+  }
 
-        const userInfo = await service.user.exist({ email });
+  async sendVerifyCode() {
+    const { ctx, service } = this;
+    const { id, email, status, name } = ctx.state.user;
 
-        const { _id, name } = userInfo;
+    if (status === "publish") return ctx.helper.error(10018);
 
-        const token = await service.utils.generateCode(_id, 7200);
+    const token = await service.utils.generateCode(id, 7200, 12);
+    const link = `${this.app.config.authUrl}?token=${token}`;
 
-        service.utils.sendMail({
-            to: email,
-            subject: '重置密码',
-            text: '正文如下:',
-            html: `<h3>亲爱的${name}:<h3><p>您正在进行重置密码的操作,如果不是您本人所为请忽略此邮件,确认重置密码请复制点击下方验证码, 有效期两小时:</p><p style='margin-left:30px;font-size:20px'>${token}</p>`,
-        });
+    service.utils.sendMail({
+      to: email,
+      subject: "账户验证",
+      text: "正文如下:",
+      html:
+        `<h3>亲爱的${name}:<h3><p>请点击下方链接进行账户验证, 有效期两小时:</p><p style='margin-left:30px;font-size:20px'>${link}</p>`,
+    });
 
-        ctx.helper.success('send success');
-    }
+    ctx.helper.success("send success");
+  }
 
-    async resetPasswordAuth() {
-        const { ctx, service } = this;
-        const { password, token } = ctx.request.body;
+  async authVerifyCode() {
+    const { ctx, service } = this;
+    const { token } = ctx.request.body;
 
-        let result = 10011;
-        try {
-            const id = await service.utils.authCode(token);
-            if (!id) return ctx.helper.error(10016);
+    const id = await service.utils.authCode(token);
+    if (!id) return ctx.helper.error(10016);
 
-            const newPass = ctx.helper.MD5(ctx.app.config.salt + password);
-            const refreshToken = uuidv4();
-            result = await service.user.update([id], { password: newPass, refreshToken }).catch(() => 10011);
-        } catch (err) {}
+    service.user.update([id], { status: "publish" });
 
-        ctx.helper.send(result);
-    }
-
-    async sendVerifyCode() {
-        const { ctx, service } = this;
-        const { id, email, status, name } = ctx.state.user;
-
-        if (status === 'publish') return ctx.helper.error(10018);
-
-        const token = await service.utils.generateCode(id, 7200, 12);
-        const link = `${this.app.config.authUrl}?token=${token}`;
-
-        service.utils.sendMail({
-            to: email,
-            subject: '账户验证',
-            text: '正文如下:',
-            html: `<h3>亲爱的${name}:<h3><p>请点击下方链接进行账户验证, 有效期两小时:</p><p style='margin-left:30px;font-size:20px'>${link}</p>`,
-        });
-
-        ctx.helper.success('send success');
-    }
-
-    async authVerifyCode() {
-        const { ctx, service } = this;
-        const { token } = ctx.request.body;
-
-        const id = await service.utils.authCode(token);
-        if (!id) return ctx.helper.error(10016);
-
-        service.user.update([id], { status: 'publish' });
-
-        ctx.helper.success('auth success');
-    }
+    ctx.helper.success("auth success");
+  }
 }
 
 export default AuthController;
