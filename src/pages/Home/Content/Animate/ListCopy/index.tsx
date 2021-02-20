003fbd5477479acfa5f5e3abfc51a5a8c94c667e
Qinmei <sun.houtao@outlook.com>
2021-02-20 15:28:19 +0800

    feature: 一些调整

--- src/pages/Home/Content/Animate/ListCopy/index.tsx
+++ src/pages/Home/Content/Animate/ListCopy/index.tsx
@@ -1,15 +1,15 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import { useModel } from "@/action";
 import { ListLayout } from "@/layouts";
 import { ListOptions, ListTable } from "@/components";
 import { getLang } from "@/locales";
 import { useColumns } from "./useColumns";
 import { QuickEditForm } from "../Common/QuickEditForm";
-import { useMethods } from "./useMethods";
-
+import { AnimateController } from "@/controllers";
 import { AnimateType, CommonType } from "@/types";
 
 import { useListLoading, useSelect } from "../Common/GlobalState";
+import { useSavedState } from "@/hooks";
 
 const initialState: CommonType.ListQuery = {
   page: 1,
@@ -27,37 +27,54 @@ const initialState: CommonType.ListQuery = {
 };
 
 const List: FC = () => {
-  const [state, methods] = useMethods(initialState);
-  const { page, size, title } = state;
-  const { init } = methods;
-
-  const [loading] = useListLoading();
+  const [query, setQuery] = useSavedState(initialState, "animate");
+  const [loading, setLoading] = useListLoading();
   const [select, setSelect] = useSelect();
+  const { title, page, size } = query;
+
+  const dispatch = (propKey: string, value: unknown) => {
+    switch (propKey) {
+      case "query":
+        setQuery(value as CommonType.ListQuery);
+        break;
+      case "select":
+        setSelect(value as string[]);
+        break;
+      case "loading":
+        setLoading(value as boolean);
+        break;
+      default:
+        break;
+    }
+  };
+  const { current: controller } = useRef(
+    new AnimateController({ query, loading, select }, dispatch)
+  );
 
   const { list, total } = useModel("animate");
-  const { columns, SettingBtn } = useColumns(state, methods);
+  const { columns, SettingBtn } = useColumns(query, controller);
 
   useEffect(() => {
-    init();
-  }, [init]);
+    controller.init();
+  }, [controller, query]);
 
   return (
     <ListLayout
       options={
         <ListOptions<Omit<AnimateType.UpdateListReq, "ids">>
           selected={select}
-          onSubmit={methods.updateMany}
+          onSubmit={controller.updateMany}
           newPath="/home/animate/add"
-          onRemove={methods.removeMany}
+          onRemove={controller.removeMany}
         >
           <QuickEditForm />
         </ListOptions>
       }
       placeholder={getLang("animate.title.search")}
       value={title}
-      onChange={title => methods.set({ title })}
+      onChange={title => controller.setQuery({ title })}
       setting={SettingBtn}
-      reset={methods.reset}
+      reset={controller.reset}
     >
       <ListTable<AnimateType.List>
         loading={loading}
@@ -68,7 +85,7 @@ const List: FC = () => {
         list={list}
         select={select}
         onSelectChange={setSelect}
-        onChange={methods.set}
+        onChange={controller.setQuery}
       />
     </ListLayout>
   );
